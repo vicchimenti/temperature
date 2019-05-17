3a885ebd876137ce3707bd4a76dc5bde4bd18445
Vic Chimenti <chimenti@seattleu.edu>
2019-05-17 10:39:00 -0700

    move angular logo to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Temperature Conversions';
+  title = 'Convert Any Temperature';
 }
